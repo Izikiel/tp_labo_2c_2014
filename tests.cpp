39f019d3ecf8585fc4f96f8e52be25caa0030ceb
Izikiel <ezequiel.gambaccini@gmail.com>
2014-09-12 14:39:23 -0300

    saque couts

--- tests.cpp
+++ tests.cpp
@@ -6,28 +6,31 @@
 
 
 template<typename T>
-string to_s(const T& m) {
- 	ostringstream os;
-	os << m;
-	return os.str();
- }
+string to_s(const T &m)
+{
+    ostringstream os;
+    os << m;
+    return os.str();
+}
 
 /*
  * Crea una mesa de int vacia y no agrega elementos
  */
-void check_crear_mesa_vacia() {
-  Arturo<int> mesa;
+void check_crear_mesa_vacia()
+{
+    Arturo<int> mesa;
 
-  ASSERT_EQ(mesa.esVacia(), true);
+    ASSERT_EQ(mesa.esVacia(), true);
 
-  ASSERT_EQ(mesa.tamanio(), 0);
+    ASSERT_EQ(mesa.tamanio(), 0);
 }
 
 
 /*
  * Crea un Arturo de Arturos.
  */
-void check_arturo_de_arturo(){
+void check_arturo_de_arturo()
+{
     Arturo<string> mesa1;
     Arturo<string> mesa2;
 
@@ -36,23 +39,17 @@ void check_arturo_de_arturo(){
     dobleMesa.sentarArturo(mesa1);
 
     mesa2.sentarArturo("ARTURO");
-    cout << mesa2 << endl;
-
-    cout << dobleMesa << endl;
-    dobleMesa.incorporarCaballero(mesa2);
-    cout << dobleMesa << endl;
 
     ASSERT(dobleMesa.caballeroActual() == mesa1);
-
-
 }
 
 
 
-int main() {
+int main()
+{
     RUN_TEST(check_crear_mesa_vacia);
     RUN_TEST(check_arturo_de_arturo);
 
-  return 0;
+    return 0;
 }
 
